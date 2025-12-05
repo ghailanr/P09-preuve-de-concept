1eb81b756a06561caf25956147f4ef4b6b29b098
benjamin <benjamin.ghailane@gmail.com>
2025-12-05 01:18:05 +0100

    test api

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,1 +1,22 @@
-import utils
+import os
+import joblib
+from fastapi import FastAPI
+from utils import download_if_needed
+
+app = FastAPI()
+
+@app.on_event("startup")
+def load_artifacts():
+    global model, tokenizer
+
+    for filename, local_path in FILES.items():
+        download_if_needed(filename, local_path)
+
+    print("\n[INFO] Chargement du modèle...")
+    model = joblib.load(FILES["model.pkl"])
+    tokenizer = joblib.load(FILES["tokenizer.pkl"])
+    print("[INFO] Modèle et tokenizer chargés.")
+
+@app.get("/")
+def root():
+    return {"status": "API ok", "cache": "enabled"}
